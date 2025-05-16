d9ea60868b8ad16bdebfb515bd1c29ac5501b0d3
Craime-Doggo <hiquegmattos@hotmail.com>
2025-05-16 19:15:28 -0300

    Add files via upload
    Não há nada de substância nesse primeiro commit

---
+++ SCE/src/principal/Principal.java
@@ -0,0 +1,10 @@
+package principal;
+
+import visao.Master;
+
+public class Principal {
+    public static void main (String[] args) {
+        Master menuprincipal = new Master();
+        menuprincipal.setVisible(true);
+    }
+}
