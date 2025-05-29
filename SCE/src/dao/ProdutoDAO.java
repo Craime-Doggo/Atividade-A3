3473d11075368c9e6fed06219c6c0ed6f209134f
AllexMaia <allexmaia2@gmail.com>
2025-05-29 19:44:43 -0300

    Update ProdutoDAO.java

--- SCE/src/dao/ProdutoDAO.java
+++ SCE/src/dao/ProdutoDAO.java
@@ -9,5 +9,22 @@ package dao;
  * @author Gabriel
  */
 public class ProdutoDAO {
+    /**
+    *
+    *
+    public Connection getConexao() {
+Connection connection = null; //instância da conexão
+try {
+// Carregamento do JDBC Driver
+String driver = "com.mysql.cj.jdbc.Driver";
+Class.forName(driver);
+// Configurar a conexão
+String server = "localhost";
+String database = "db_alunos";
+String url = "jdbc:mysql://" + server + ":3306/"
++ database + "?useTimezone=true&serverTimezone=UTC";
+String user = "root";
+String password = "root";
+    **/
     
 }
