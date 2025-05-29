c22958464268045bb59ba5adb36d74efbd18e387
AllexMaia <allexmaia2@gmail.com>
2025-05-29 19:45:10 -0300

    Update ProdutoDAO.java

--- SCE/src/dao/ProdutoDAO.java
+++ SCE/src/dao/ProdutoDAO.java
@@ -25,6 +25,24 @@ String url = "jdbc:mysql://" + server + ":3306/"
 + database + "?useTimezone=true&serverTimezone=UTC";
 String user = "root";
 String password = "root";
+
+
+// Conectando..
+connection = DriverManager.getConnection(url, user, password);
+// Testando..
+if (connection != null) {
+System.out.println("Status: Conectado!");
+} else {
+System.out.println("Status: NÃO CONECTADO!");
+}
+return connection;
+} catch (ClassNotFoundException e) { //Driver não encontrado
+System.out.println("O driver nao foi encontrado.");
+return null;
+} catch (SQLException e) {
+System.out.println("Nao foi possivel conectar...");
+return null;
+}
     **/
     
 }
