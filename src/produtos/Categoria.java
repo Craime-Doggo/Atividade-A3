92594dc35a24ab62bd0c0d0baad2357b42595378
AllexMaia <allexmaia2@gmail.com>
2025-06-01 14:24:08 -0300

    atualizado

--- src/produtos/Categoria.java
+++ src/produtos/Categoria.java
@@ -30,59 +30,46 @@ public class Categoria {
     }
     
     
-    /**
-     * @return the id_categoria
-     */
+
     public int getId_categoria() {
         return id_categoria;
     }
 
-    /**
-     * @param id_categoria the id_categoria to set
-     */
     public void setId_categoria(int id_categoria) {
         this.id_categoria = id_categoria;
     }
 
-    /**
-     * @return the nome_categoria
-     */
     public String getNome_categoria() {
         return nome_categoria;
     }
 
-    /**
-     * @param nome_categoria the nome_categoria to set
-     */
     public void setNome_categoria(String nome_categoria) {
         this.nome_categoria = nome_categoria;
     }
 
-    /**
-     * @return the tamanho
-     */
     public String getTamanho() {
         return tamanho;
     }
 
-    /**
-     * @param tamanho the tamanho to set
-     */
     public void setTamanho(String tamanho) {
         this.tamanho = tamanho;
     }
 
-    /**
-     * @return the embalagem
-     */
     public String getEmbalagem() {
         return embalagem;
     }
 
-    /**
-     * @param embalagem the embalagem to set
-     */
+
     public void setEmbalagem(String embalagem) {
         this.embalagem = embalagem;
     }
+@Override
+    public String toString() {
+    return "id_categoria = " + id_categoria +
+           ", nome_categoria = " + nome_categoria +
+           ", tamanho = " + tamanho +
+           ", embalagem = " + embalagem +
+           ", user = root" +
+           ", password = admin";
+        }
 }
