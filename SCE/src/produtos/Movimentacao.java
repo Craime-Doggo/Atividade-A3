499a4efd0899e0f775c5ff97d1b916e640633777
kaue-santana <kauesantana2308@gmail.com>
2025-06-03 15:44:28 -0300

    Produtos - Movimentação
    Tive que adicionar uma movimentação em produtos, pra ver se funciona mesmo.

---
+++ SCE/src/produtos/Movimentacao.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package produtos;
+
+/**
+ *
+ * @author parto
+ */
+public class Movimentacao {
+
+    private int idProduto;
+    private String tipo; // "entrada" ou "retirada"
+    private int quantidade;
+
+    public Movimentacao(int idProduto, String tipo, int quantidade) {
+        this.idProduto = idProduto;
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Movimentacao{" +
+                "idProduto=" + idProduto +
+                ", tipo='" + tipo + '\'' +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+} 
+
+
